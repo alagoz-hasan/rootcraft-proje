67b4a75f093d95b5794028534f4d239c1a2abebe
alagoz-hasan <hasanalagozcs@gmail.com>
2021-01-04 22:24:28 +0300

    Update GridManager.cs

--- RootcraftEditor/Assets/Scripts/GridManager.cs
+++ RootcraftEditor/Assets/Scripts/GridManager.cs
@@ -31,7 +31,7 @@ public class GridManager : MonoBehaviour
                 circle.GetComponent<SpriteRenderer>().color = Color.gray;
                 circles[i].Add(circle);
             }
-            
+
         }
     }
     private void Update()
@@ -46,7 +46,7 @@ public class GridManager : MonoBehaviour
             {
                 GameObject circle = circles[circles.Count - 1][i];
                 Destroy(circle);
-                
+
             }
             circles.RemoveAt(circles.Count - 1);
 
@@ -70,9 +70,9 @@ public class GridManager : MonoBehaviour
             }
             Vector3 scaleFactor = circles[0][0].transform.localScale - new Vector3(0.08f, 0.08f, 0.08f);
             Vector3 position = new Vector3(0f, 1f, 0f);
-           
+
             Vector3 a = new Vector3(0f, 0.2f, 0f);
-            ScaleGridY(scaleFactor,position,a);
+            ScaleGridY(scaleFactor, position, a);
         }
         if (left)
         {
@@ -82,39 +82,39 @@ public class GridManager : MonoBehaviour
             {
                 GameObject circle = circles[i][columns - 1];
                 Destroy(circle);
-                circles[i].RemoveAt(columns-1);
+                circles[i].RemoveAt(columns - 1);
             }
 
             Vector3 scaleFactor = circles[0][0].transform.localScale + new Vector3(0.08f, 0.08f, 0.08f);
             Vector3 position = new Vector3(1f, 0f, 0f);
             Vector3 a = new Vector3(-0.4f, 0f, 0f);
-            ScaleGridX(scaleFactor, position,a);
+            ScaleGridXLeft(scaleFactor, position, a);
         }
         if (right)
         {
             right = false;
             int count = circles.Count;
-            Vector3 pos = circles[0][columns-2].transform.position - circles[0][columns-1].transform.position;
+            Vector3 pos = circles[0][columns - 2].transform.position - circles[0][columns - 1].transform.position;
             pos.y = 0f;
             pos.z = 0f;
             for (int i = 0; i < count; i++)
-            { 
-                GameObject circle = Instantiate(prefab, circles[i][columns-1].transform.position - pos, Quaternion.identity);
+            {
+                GameObject circle = Instantiate(prefab, circles[i][columns - 1].transform.position - pos, Quaternion.identity);
                 circle.GetComponent<SpriteRenderer>().color = Color.gray;
                 circles[i].Add(circle);
             }
             Vector3 scaleFactor = circles[0][0].transform.localScale - new Vector3(0.08f, 0.08f, 0.08f);
             Vector3 position = new Vector3(-1f, 0f, 0f);
             Vector3 a = new Vector3(0.4f, 0f, 0f);
-            ScaleGridX(scaleFactor, position, a);
+            ScaleGridXRight(scaleFactor, position, a);
         }
     }
-    
+
     public void ScaleGridY(Vector3 scaleFactor, Vector3 position, Vector3 pos)
     {
-        for(int i = 0; i < circles.Count; i++)
+        for (int i = 0; i < circles.Count; i++)
         {
-            for(int j = 0; j< circles[0].Count; j++)
+            for (int j = 0; j < circles[0].Count; j++)
             {
                 circles[i][j].transform.localScale = scaleFactor;
                 circles[i][j].transform.position += position;
@@ -129,7 +129,7 @@ public class GridManager : MonoBehaviour
         }
 
     }
-    public void ScaleGridX(Vector3 scaleFactor, Vector3 position, Vector3 pos)
+    public void ScaleGridXRight(Vector3 scaleFactor, Vector3 position, Vector3 pos)
     {
         for (int i = 0; i < circles.Count; i++)
         {
@@ -152,7 +152,34 @@ public class GridManager : MonoBehaviour
         {
             for (int j = 1; j < circles[0].Count; j++)
             {
-                circles[i][j].transform.position -= new Vector3(0.15f,0f,0f) * j;
+                circles[i][j].transform.position -= new Vector3(0.15f, 0f, 0f) * j;
+            }
+        }
+    }
+    public void ScaleGridXLeft(Vector3 scaleFactor, Vector3 position, Vector3 pos)
+    {
+        for (int i = 0; i < circles.Count; i++)
+        {
+            for (int j = 0; j < circles[0].Count; j++)
+            {
+                circles[i][j].transform.localScale = scaleFactor;
+                circles[i][j].transform.position += position;
+            }
+        }
+        int column = circles[0].Count;
+        for (int i = 0; i < circles.Count; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                circles[i][j].transform.position += pos;
+            }
+
+        }
+        for (int i = 0; i < circles.Count; i++)
+        {
+            for (int j = 1; j < circles[0].Count; j++)
+            {
+                circles[i][j].transform.position += new Vector3(0.15f, 0f, 0f) * j;
             }
         }
     }
@@ -163,11 +190,11 @@ public class GridManager : MonoBehaviour
     }
     public void SaveData()
     {
-        string json="";
+        string json = "";
         List<GridData> datas = new List<GridData>();
-        for(int i = 0; i < circles.Count; i++)
+        for (int i = 0; i < circles.Count; i++)
         {
-            for(int j = 0; j < circles[0].Count; j++)
+            for (int j = 0; j < circles[0].Count; j++)
             {
                 GridData data = new GridData();
                 data.position = circles[i][j].transform.position;
